07aab85e8cafc01aef87a37d320074eaf46494af
Sourav De <souravde@souravs-mbp.fios-router.home>
2024-09-06 16:41:41 -0400

    feat: application code with k8s support

--- src/controllers/controller.ts
+++ src/controllers/controller.ts
@@ -1,19 +1,15 @@
 import {Request, Response, NextFunction} from 'express';
-import {connection} from '../config/db';
+import {connectionPool} from '../config/db';
 import {OntologyConcept} from '../models/concept-interface'
+import { validate } from '../util/data-validation';
 
 //  GET all concepts in the database on-man
 export const getAllConcepts = async (req: Request, res: Response, next: NextFunction) => {
     try {
         console.log(`INFO: get all concepts in the database`);
         const sql: string = "SELECT ID, parent, child, alternateName, concept FROM `clinical-concept`;"
-        connection.connect(function(err) {
-            if (err) next(err);
-                connection.query(sql, function (err, result, fields) {
-                    if (err) next(err);
-                    res.json(result);
-            });
-        });
+        let result = await connectionPool.execute(sql);
+        res.status(200).json(result[0]);
     } catch (error) {
         next(error);
     }
@@ -26,26 +22,30 @@ export const getConceptByID = async (req: Request, res: Response, next: NextFunc
 try {
     console.log(`INFO: get concept: ${req.params.id} in the database`);
     const sql: string = "SELECT ID, parent, child, alternateName, concept FROM `clinical-concept` where ID = " + req.params.id + ";"
-    connection.connect(function(err) {
-        if (err) next(err);
-            connection.query(sql, function (err, result, fields) {
-                if (err) next(err);
-                if (JSON.stringify(result) == "[]") {
-                    res.json({
-                        "description": "No records found for the ID: " + req.params.id + " specified in the database"
-                    })
-                }
-                else res.json(result);
-        });
-    });
+    let result = await connectionPool.execute(sql);
+    
+    if (JSON.stringify(result[0]) == "[]") {
+        res.status(201).json({
+            "description": "No records found for the ID: " + req.params.id + " specified in the database"
+        })
+    }
+    else res.status(200).json(result[0]);
 } catch (error) {
     next(error);
 }
 };
 
-//   createConcept: POST
+//   POST a concept in the database on-man
 export const createConceptByID = async (req: Request, res: Response, next: NextFunction) => {
     try {
+        // only allow admin to perform create
+        if (req.headers['x-role'] != 'admin') {
+            console.log(`INFO: unauthorized attempt to create concept: ${req.body.ID}`);
+            res.status(401).json({
+                "message": "User Unauthorized"
+            });
+            return;
+        }
         
         const concept:OntologyConcept = {
             id: req.body.ID,
@@ -54,22 +54,83 @@ export const createConceptByID = async (req: Request, res: Response, next: NextF
             child: req.body.child,
             alternateName: req.body.alternateName
         }
+
+        let isValid = await validate(req);
+        if ( isValid == false) {
+            console.log(`INFO: data validation error for concept: ${req.body.ID}`);
+            res.status(501).json({
+                "message": "Data validation error"
+            });
+            return;
+        }
+
         console.log(`INFO: create concept ${concept.id} in the database`);
         // const concept: Concept = new Concept(conceptObj);
         const sql: string = "INSERT INTO `clinical-concept` (ID, concept, parent, child, alternateName) VALUES(" + `'${concept.id}', '${concept.concept}', '${concept.parent}', '${concept.child}', '${concept.alternateName}');`
-        
-        connection.connect(function(err) {
-            if (err) next(err);
-                connection.query(sql, function (err, result, fields) {
-                    if (err) next(err);
-                    else res.json(result);
-            });
-        });
+        let result = await connectionPool.execute(sql);
+        res.status(200).json(result[0]);
     } catch (error) {
         next(error);
     }
 };
 
 
-//   updateConcept: PUT
-//   deleteConcept: DELETE
+//   PUT an existing concept in the database on-man
+export const updateConceptByID = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        // only allow admin to perform update
+        if (req.headers['x-role'] != 'admin') {
+            console.log(`INFO: unauthorized attempt to update concept: ${req.body.ID}`);
+            res.status(401).json({
+                "message": "User Unauthorized"
+            });
+            return;
+        }
+
+        let isValid = await validate(req);
+        if (isValid == false) {
+            console.log(`INFO: data validation error for concept: ${req.body.ID}`);
+            res.status(501).json({
+                "message": "Data validation error"
+            });
+            return;
+        }
+        
+        const concept:OntologyConcept = {
+            id: req.body.ID,
+            concept: req.body.concept,
+            parent: req.body.parent,
+            child: req.body.child,
+            alternateName: req.body.alternateName
+        }
+        console.log(`INFO: update concept ${concept.id} in the database`);
+        const sql: string = "UPDATE `clinical-concept` SET concept = " + `'${concept.concept}'` + ", parent = " + `'${concept.parent}'` + ", child = " + `'${concept.child}'` + ", alternateName = " + `'${concept.alternateName}'` + " WHERE ID = " + `'${concept.id}';`
+        let result = await connectionPool.execute(sql);
+        res.status(200).json(result[0]);
+    } catch (error) {
+        next(error);
+    }
+};
+
+
+//   DELETE an existing concept in the database on-man
+export const deleteConceptByID = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        // only allow admin to perform delete
+        if (req.headers['x-role'] != 'admin') {
+            console.log(`INFO: unauthorized attempt to delete concept: ${req.params.id}`);
+            res.status(401).json({
+                "message": "User Unauthorized"
+            });
+            return;
+        }
+
+        console.log(`INFO: delete concept: ${req.params.id} in the database`);
+        
+        const sql: string = "DELETE FROM `clinical-concept` WHERE ID = " + `'${req.params.id}';`
+        let result = await connectionPool.execute(sql);
+        res.status(200).json(result[0]);
+    } catch (error) {
+        next(error);
+    }
+};
---
+++ src/util/data-validation.ts
@@ -0,0 +1,48 @@
+import { connectionPool } from "../config/db";
+import { Request, Response, NextFunction} from 'express';
+
+// All the data validation is done here before performing database operation
+export async function validate(req: Request): Promise<boolean> {
+    console.log(`validating concept: ${req.body.ID}`)
+
+    if (req.method == 'POST' || req.method == 'PUT') {
+        // validation #1: concept cannot be it's own child
+        if (req.body.child?.split(",").includes(req.body.ID.toString())) {
+            console.log(`concept cannot be it's own child`)
+            return false;
+        }
+
+        // validation #2: concept cannot be it's own parent
+        if (req.body.parent?.split(",").includes(req.body.ID.toString())) {
+            console.log(`concept cannot be it's own parent`)
+            return false;
+        }
+
+        // validation #3: if parent != '', concept cannot have a non-existing parent
+        if (req.body.parent != '') {
+            let parentSQL: string = "'" + req.body.parent.split(",").join("','") + "'";
+            const sql: string = "SELECT ID FROM `clinical-concept` WHERE ID IN (" + parentSQL + ");"
+            
+            let result: any = await connectionPool.execute(sql);
+            if (result[0].length != req.body.parent.split(",").length) {
+                console.log(`concept cannot claim a non-existent parent`)
+                return false;
+            }
+        }
+
+        // validation #4: if child != '', concept cannot have a non-existing child
+        if (req.body.child != '') {
+            let childSQL: string = "'" + req.body.child.split(",").join("','") + "'";
+            const sql: string = "SELECT ID FROM `clinical-concept` WHERE ID IN (" + childSQL + ");"
+            
+            let result: any = await connectionPool.execute(sql);
+            if (result[0].length != req.body.child.split(",").length) {
+                console.log(`concept cannot claim a non-existent child`)
+                return false;
+            }
+        }
+        
+    }
+
+    return true;
+}
